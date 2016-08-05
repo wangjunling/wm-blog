6be58feedbe1da962c4ffffeacd6b322fc7d65ca
wangjunling <wangjunling>
2016-08-05 18:18:38 +0800

    index

--- src/main/java/com/wmblog/controller/IndexController.java
+++ src/main/java/com/wmblog/controller/IndexController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController
 {
-    @RequestMapping("/index")
+    @RequestMapping("")
     public String hello()
     {
         return "index";
