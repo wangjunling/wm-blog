86dcc602806c8bda758a0a28c30db8ddb8679d5a
wangjunling <wangjunling>
2016-08-25 14:27:59 +0800

    用户首页前端样式

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,12 +1,16 @@
 /**
  * Created by wangjunling on 2016/8/15.
  */
+
 var app = angular.module("indexApp", ['ngRoute', 'ngSanitize']);
 
 app.config(function ($routeProvider) {
     $routeProvider.when('/', {
         controller: listController,
         templateUrl: 'list.html'
+    }).when("/home/:user", {
+        controller: userController,
+        templateUrl: 'user.html'
     }).when("/detail/:id", {
         controller: detailController,
         templateUrl: 'detail.html'
@@ -109,4 +113,8 @@ function writeController($scope, $http) {
 }
 function saveResultController() {
     
+}
+function userController() {
+    $('ul.tabs').tabs();
+    $('.materialboxed').materialbox();
 }
